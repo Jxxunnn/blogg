7561e04517ff8800bddcf3a9146604465a84af2f
Jxxunnn <86228307+Jxxunnn@users.noreply.github.com>
2022-02-08 15:37:31 +0900

    basic react reviewing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  let [글제목, 글제목변경] = useState("남자 코트 추천");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav--black">
+        <div>개발 blog</div>
+      </div>
+      <div className="list">
+        <h3>{글제목}</h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
     </div>
   );
 }
